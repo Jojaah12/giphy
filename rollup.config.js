b20060bafaf954d3507eb442be62ddd5559c0d75
Jojaah12 <mohammed.aljojah@gmail.com>
2021-03-21 13:18:05 +0100

    Add live-reloading scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,17 @@
+import browsersync from 'rollup-plugin-browsersync'
+const isProduction = process.env.NODE_ENV === 'production';
+const isDevelopment = isProduction === false;
 export default {
     input: 'src/scripts/index.js',
     output: {
         file: 'public/giphy.js',
         format: 'iife'
-    }
+    },
+
+    plugins: [
+        isDevelopment && browsersync({
+            server: 'public'
+        })
+    ]
+
 };
